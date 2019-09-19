86a9c3ac4ee614e784e52e946c8a76039ce71b30
Chris Pratt <croxxp@gmail.com>
2019-09-18 20:59:40 -0400

    Basic easy-peasy stuff.

--- src/game/index.tsx
+++ src/game/index.tsx
@@ -1,6 +1,24 @@
 import app from "./app";
-import React from "react"
+import React, { Fragment } from "react";
+
+import { useStoreState, useStoreActions } from "easy-peasy";
 
 export default () => {
-  return (<div ref={(nodeElement) => {nodeElement && nodeElement.appendChild(app.view)}}></div>)
-}
+  const volume = useStoreState(state => state.volume.level);
+  const updateVolume = useStoreActions(
+    actions => (actions as any).volume.change
+  );
+
+  return (
+    <Fragment>
+      <div
+        ref={nodeElement => {
+          nodeElement && nodeElement.appendChild(app.view);
+        }}
+      ></div>
+      <div style={{ position: "absolute", bottom: "0px", right: "0px" }}>
+        <button onClick={() => updateVolume(5)}>{volume}</button>
+      </div>
+    </Fragment>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,20 @@ import React from "react";
 import { render } from "react-dom";
 import Game from "./game";
 
-render(<Game />, document.getElementById("root"));
+import { createStore, action, StoreProvider } from "easy-peasy";
+
+const store = createStore({
+  volume: {
+    level: 0.3,
+    change: action((state, payload) => {
+      (state as any).level = payload;
+    })
+  }
+});
+
+render(
+  <StoreProvider store={store}>
+    <Game />
+  </StoreProvider>,
+  document.getElementById("root")
+);
